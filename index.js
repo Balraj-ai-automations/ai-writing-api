46899dc4cfdcab2f5e79f4e68fa56a47c9b756c5
Balraj S <your-email@example.com>
2026-05-02 00:09:13 +0530

    deploy backend

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ app.post("/improve-text", async (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
